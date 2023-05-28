95aa5595486c585d9384a42de114896ec4cbc059
Abhi-Coder722 <072abhi@gmail.com>
2023-05-28 16:38:53 +0200

    it works :)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var posts=[];
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -42,11 +43,27 @@ app.post("/compose",function(req,res){
   res.redirect("/");
 })
 
+// expressing the route parameters for /posts and get the name entered by the user stored in the variable called postName
+app.get("/posts/:postName", function(req,res){
+  var requestedTitle=req.params.postName;
+  requestedTitle=_.lowerCase(requestedTitle);
+  requestedTitle = _.kebabCase(requestedTitle);
+  posts.forEach(function(post){
+    var storedTitle= post.title;
+    storedTitle=_.lowerCase(storedTitle);
+    storedTitle = _.kebabCase(storedTitle);
 
 
+    if(storedTitle === requestedTitle){
+      res.render("post", { 
+        title: post.title,
+        content : post.post
+      });//to render the post page
+    }
+  });
+})
 
-
-
+// using rs on our terminal while running app.js using nodemon will restart the nodemon app.js and clear the virtual changes made to the application 
 
 
 
